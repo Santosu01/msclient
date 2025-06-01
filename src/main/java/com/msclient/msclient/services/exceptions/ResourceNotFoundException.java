2cc78c6b44acc1e8a3386b4e7702a4e4178b1430
Marcio Santos <santosu01@gmail.com>
2025-06-01 17:49:59 -0300

    feat: add resource exception service

---
+++ src/main/java/com/msclient/msclient/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.msclient.msclient.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
